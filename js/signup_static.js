9a85592b1fb6d390bb42417311c9561a1876f3c8
Elliot J. Voris <elliot@voris.me>
2015-08-25 09:17:59 -0500

    We don't need this space!!
    Looks dumb, man!

--- js/signup_static.js
+++ js/signup_static.js
@@ -63,7 +63,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
